133a34a2fcbce8f91a79711af5a51979b9ab23c0
ArseniyK1 <qwertototo2332@gmail.com>
2024-02-25 15:23:32 +0300

    Добавление юзера и роли

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { RolesModule } from './roles/roles.module';
+import { RequestModule } from './request/request.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { CaseModule } from './case/case.module';
+import { databaseProviders } from './db/database.providers';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
+    UserModule,
+    RolesModule,
+    RequestModule,
+    PortfolioModule,
+    CaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
+  exports: [...databaseProviders],
 })
 export class AppModule {}
+//
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  const PORT = process.env.PORT || 4400;
+
+  const config = new DocumentBuilder()
+    .setTitle('Юридическая помощь онлайн')
+    .setDescription('Веб-приложение для онлайн связи клиентов с юристами')
+    .setVersion('0.0.1')
+    .addTag('Arseniy Kiselev')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () =>
+    console.log(`Сервер запустился на ${PORT} порту`),
+  );
 }
 bootstrap();
