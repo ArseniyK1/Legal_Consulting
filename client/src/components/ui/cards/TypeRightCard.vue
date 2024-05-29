3ba6660be0c756cf976807db33898cac35243a60
ArseniyK1 <qwertototo2332@gmail.com>
2024-05-29 20:02:05 +0300

    feat: дизайн карточки с типом права

--- client/src/components/ui/cards/TypeRightCard.vue
+++ client/src/components/ui/cards/TypeRightCard.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-card class="my-card shadow-3 row">
+  <q-card class="my-card shadow-3 column justify-between" style="height: 100%">
     <q-card-section class="bg-info text-white">
       <div class="text-h6">{{ name }}</div>
       <div class="text-subtitle2">{{ description }}</div>
     </q-card-section>
 
-    <q-card-actions align="center">
-      <slot name="type-trouble"></slot>
-    </q-card-actions>
+    <q-card-section class="col q-pa-none overflow-hidden">
+      <div class="q-gutter-sm scroll">
+        <slot name="type-trouble"></slot>
+      </div>
+    </q-card-section>
   </q-card>
 </template>
 
@@ -47,3 +49,10 @@ const props = defineProps({
 
 // LIFECYCLE HOOKS
 </script>
+
+<style scoped>
+.scroll {
+  overflow-y: auto;
+  height: 100%;
+}
+</style>
