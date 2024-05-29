197c378a8af28502a37fb65c1d2da7aea7defed4
ArseniyK1 <qwertototo2332@gmail.com>
2024-05-29 20:19:15 +0300

    feat: дизайн карточки с типом права

--- client/src/components/ui/cards/TypeRightCard.vue
+++ client/src/components/ui/cards/TypeRightCard.vue
@@ -1,12 +1,15 @@
 <template>
-  <q-card class="my-card shadow-3 column justify-between" style="height: 100%">
+  <q-card
+    class="my-card shadow-3 column justify-between"
+    style="min-height: 300px; max-height: 600px"
+  >
     <q-card-section class="bg-info text-white">
       <div class="text-h6">{{ name }}</div>
       <div class="text-subtitle2">{{ description }}</div>
     </q-card-section>
 
-    <q-card-section class="col q-pa-none overflow-hidden">
-      <div class="q-gutter-sm scroll">
+    <q-card-section class="q-pa-none trouble-types-section">
+      <div class="trouble-types">
         <slot name="type-trouble"></slot>
       </div>
     </q-card-section>
@@ -34,25 +37,30 @@ const props = defineProps({
     default: () => [],
   },
 });
-
-// INJECTABLE
-// INJECTABLE
-
-// REFS
-// REFS
-
-// COMPUTED
-
-// METHODS
-
-// WATCH
-
-// LIFECYCLE HOOKS
 </script>
 
 <style scoped>
-.scroll {
-  overflow-y: auto;
-  height: 100%;
+.my-card {
+  display: flex;
+  flex-direction: column;
+}
+
+.bg-info {
+  flex: 0 0 auto;
+}
+.trouble-types-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1 0;
+  height: 100% !important;
+}
+
+.trouble-types {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
 }
 </style>
